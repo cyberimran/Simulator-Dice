fd7cffaf9c9300c8c96181d71041be4f77f4ef0d
MOHAMMAD IMRAN <120778498+imrancoding@users.noreply.github.com>
2023-01-25 05:08:16 +0530

    Update dicethree.py

--- Dices/dicethree.py
+++ Dices/dicethree.py
@@ -3,7 +3,7 @@ def dice_three():
     """
     If you use this dice then there will be more chances of getting 3.
     """
-    num=[1,2,3,4,5,6,3,3]
+    num=[1, 2, 3, 4, 5, 6, 3, 3]
     i=random.choice(num)
     return f"""
   ---------
