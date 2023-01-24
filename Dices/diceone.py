05d89c4b96d6edd0153c5f0e7f2ed76267de151d
MOHAMMAD IMRAN <120778498+imrancoding@users.noreply.github.com>
2023-01-25 05:06:56 +0530

    Update diceone.py

--- Dices/diceone.py
+++ Dices/diceone.py
@@ -3,7 +3,7 @@ def dice_one():
     """
     If you use this dice then there will be more chances of getting 1.
     """
-    num=[1,2,3,5,6,1,1]
+    num=[1, 2, 3, 5, 6, 1, 1]
     i=random.choice(num)
     return f"""
   ---------
