ff141b397e586ed852a396905509c50525683c5d
MOHAMMAD IMRAN <120778498+imrancoding@users.noreply.github.com>
2023-01-25 05:05:49 +0530

    Update dicefive.py

--- Dices/dicefive.py
+++ Dices/dicefive.py
@@ -3,7 +3,7 @@ def dice_five():
     """
     If you use this dice then there will be more chances of getting 5.
     """
-    num=[1,2,3,4,5,6,5,5]
+    num=[1, 2, 3, 4, 5, 6, 5, 5]
     i=random.choice(num)
     return f"""
   ---------
