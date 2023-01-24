cb60a100506be34aeaae4c6a65f2008876f3737b
MOHAMMAD IMRAN <120778498+imrancoding@users.noreply.github.com>
2023-01-25 05:07:41 +0530

    Update dicesix.py

--- Dices/dicesix.py
+++ Dices/dicesix.py
@@ -3,7 +3,7 @@ def dice_six():
     """
     If you use this dice then there will be more chances of getting 6.
     """
-    num=[1,2,3,4,5,6,6,6]
+    num=[1, 2, 3, 4, 5, 6, 6, 6]
     i=random.choice(num)
     return f"""
   ---------
